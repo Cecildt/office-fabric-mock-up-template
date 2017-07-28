9e5af00dd3e87bae777f339d637044dbb16c3fc5
cecildt <cecildt@gmail.com>
2017-07-28 09:41:37 +0200

    Initial commit. Setup final.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # office-fabric-mock-up-template
 Template project to create mock-ups using Office UI Fabric React framework
+
+
+* To build components: npm run build
+* To build component for production: npm run prod
+* To watch build: npm run watch
+
+Use Web Server for Chrome extension in Chrome to browse index.html
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
+
+const MyPage = () => (<Fabric><DefaultButton>I am a button.</DefaultButton></Fabric>);
+
+ReactDOM.render(<MyPage />, document.getElementById("app"));
